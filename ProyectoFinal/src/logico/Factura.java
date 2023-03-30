d08e9fa9a2f21439ca3000463424ed7d6901998f
Cheyla Ruiz <Cheyla Ruiz@DESKTOP-G26SF7M>
2023-03-30 13:59:29 -0400

    Parámetros y Constructor de Clase Tienda y Factura

--- ProyectoFinal/src/logico/Factura.java
+++ ProyectoFinal/src/logico/Factura.java
@@ -1,5 +1,54 @@
 package logico;
 
-public class Factura {
+import java.util.ArrayList;
 
+public class Factura {
+	
+	private String codigo;
+	private Cliente cliente;
+	private ArrayList<Combo> misCombos;
+	private ArrayList<Componente> misComponentes;
+	
+	public Factura(String codigo, Cliente cliente, ArrayList<Combo> misCombos, ArrayList<Componente> misComponentes) {
+		super();
+		this.codigo = codigo;
+		this.cliente = cliente;
+		this.misCombos = misCombos;
+		this.misComponentes = misComponentes;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public ArrayList<Combo> getMisCombos() {
+		return misCombos;
+	}
+
+	public void setMisCombos(ArrayList<Combo> misCombos) {
+		this.misCombos = misCombos;
+	}
+
+	public ArrayList<Componente> getMisComponentes() {
+		return misComponentes;
+	}
+
+	public void setMisComponentes(ArrayList<Componente> misComponentes) {
+		this.misComponentes = misComponentes;
+	}
+	
+ 
+	
 }
--- ProyectoFinal/src/logico/Tienda.java
+++ ProyectoFinal/src/logico/Tienda.java
@@ -1,7 +1,64 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Tienda {
 
-	//Welcome to our Store!!
+	private ArrayList<Cliente> misClientes;
+	private ArrayList<Combo> misCombos;
+	private ArrayList<Componente> misComponentes;
+	private ArrayList<Factura> misFacturas;
+	public static Tienda tienda = null;
+	
+	
+	public Tienda() {
+		super();
+		misClientes = new ArrayList<>();
+		misCombos = new ArrayList<>();
+		misComponentes = new ArrayList<>();
+		misFacturas = new ArrayList<>();
+		
+	}
+	
+	public static Tienda getInstance(){
+		if(tienda==null){
+			tienda = new Tienda();
+		}
+		return tienda;
+	}
+
+	public ArrayList<Cliente> getMisClientes() {
+		return misClientes;
+	}
+
+	public void setMisClientes(ArrayList<Cliente> misClientes) {
+		this.misClientes = misClientes;
+	}
+
+	public ArrayList<Combo> getMisCombos() {
+		return misCombos;
+	}
+
+	public void setMisCombos(ArrayList<Combo> misCombos) {
+		this.misCombos = misCombos;
+	}
+
+	public ArrayList<Componente> getMisComponentes() {
+		return misComponentes;
+	}
+
+	public void setMisComponentes(ArrayList<Componente> misComponentes) {
+		this.misComponentes = misComponentes;
+	}
+
+	public ArrayList<Factura> getMisFacturas() {
+		return misFacturas;
+	}
+
+	public void setMisFacturas(ArrayList<Factura> misFacturas) {
+		this.misFacturas = misFacturas;
+	}
+	
+	
 	
 }
