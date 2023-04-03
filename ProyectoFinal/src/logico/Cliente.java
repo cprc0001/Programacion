3af74a26b1570bdbfa498c7b14c1dcd796a6c78e
Cheyla Ruiz <Cheyla Ruiz@DESKTOP-G26SF7M>
2023-04-03 10:23:07 -0400

    Actualización parámetros clase Cliente

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -7,6 +7,46 @@ public class Cliente {
 	private String telefono;
 	private String direccion;
 	
-	//Corrigiendo error
+	public Cliente(String cedula, String nombre, String telefono, String direccion) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
 
 }
