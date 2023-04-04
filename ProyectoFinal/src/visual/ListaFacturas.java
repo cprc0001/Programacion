97338c8c7071b58c6cf7366402b290787fb30d38
Lesly <Lesly@DESKTOP-51189P8>
2023-04-04 05:13:41 -0400

    Programacion de clase Lista de Facturas y programacion de la parte visual de Principal

--- ProyectoFinal/src/visual/ListaFacturas.java
+++ ProyectoFinal/src/visual/ListaFacturas.java
@@ -2,15 +2,31 @@ package visual;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.text.DecimalFormat;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import logico.Factura;
+import logico.Tienda;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ListaFacturas extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private DefaultTableModel model;
+	private Object rows[];
+	//private DecimalFormat df = new DecimalFormat("0.00");
 
 	/**
 	 * Launch the application.
@@ -29,27 +45,63 @@ public class ListaFacturas extends JDialog {
 	 * Create the dialog.
 	 */
 	public ListaFacturas() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Listado de Facturas");
+		setBounds(100, 100, 677, 330);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(new BorderLayout(0, 0));
+			{
+				JScrollPane scrollPane = new JScrollPane();
+				panel.add(scrollPane, BorderLayout.CENTER);
+				{
+					String[] headers = {"Cédula","Cliente","Cantidad de componentes","Total"}; //Aún no sé si solo se definirá esto en la lista o serán más cosas
+
+					table = new JTable();
+					table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+					scrollPane.setViewportView(table);
+
+					model = new DefaultTableModel();
+					model.setColumnIdentifiers(headers);
+					table.setModel(model);
+				}
+			}
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
 			}
 		}
+		loadFacturas();
 	}
 
+	//Aún se debe modificar esto, por eso lo comenté, lo planteé para avanzar un poco
+	
+	private void loadFacturas () {
+		model.setRowCount(0);
+		rows = new Object[model.getColumnCount()];
+		for (Factura aux : Tienda.getInstance().getMisFacturas()) {
+			rows[0] = aux.getCodigo();
+			rows[1] = aux.getCliente().getNombre();
+			//rows[3] = df.format(aux.PrecioFactura());
+			//rows[2] = aux.CantComponentes();
+			model.addRow(rows);
+		}
+
+	}
 }
--- ProyectoFinal/src/visual/Principal.java
+++ ProyectoFinal/src/visual/Principal.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Principal extends JFrame {
 
@@ -31,8 +34,57 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setTitle("Menú Principal - CheyleTech Corporation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 400);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnComponentes = new JMenu("Componentes");
+		menuBar.add(mnComponentes);
+		
+		JMenuItem mntmListadoDeComponentes = new JMenuItem("Componentes disponibles");
+		mnComponentes.add(mntmListadoDeComponentes);
+		
+		JMenu mnCombos = new JMenu("Combos");
+		menuBar.add(mnCombos);
+		
+		JMenuItem mntListaDeCombos = new JMenuItem("Combos disponibles");
+		mnCombos.add(mntListaDeCombos);
+		
+		JMenu mnClientes = new JMenu("Clientes");
+		menuBar.add(mnClientes);
+		
+		JMenuItem mntmListadoDeClientes = new JMenuItem("Listado de Clientes");
+		mnClientes.add(mntmListadoDeClientes);
+		
+		JMenu mnVentas = new JMenu("Ventas");
+		menuBar.add(mnVentas);
+		
+		JMenuItem mntmFacturar = new JMenuItem("Facturar");
+		mnVentas.add(mntmFacturar);
+		
+		JMenuItem mntmListadoDeFacturas = new JMenuItem("Listado de Facturas");
+		mnVentas.add(mntmListadoDeFacturas);
+		
+		JMenu mnAdministrativo = new JMenu("Administración");
+		menuBar.add(mnAdministrativo);
+		
+		JMenuItem mntmReporteDeClientes = new JMenuItem("Reporte de Clientes");
+		mnAdministrativo.add(mntmReporteDeClientes);
+		
+		JMenuItem mntmReporteDeComponentes = new JMenuItem("Reporte de Componentes");
+		mnAdministrativo.add(mntmReporteDeComponentes);
+		
+		JMenuItem mntmReporteDeVentas = new JMenuItem("Reporte de Ventas");
+		mnAdministrativo.add(mntmReporteDeVentas);
+		
+		JMenuItem mntmRegistrarComponentes = new JMenuItem("Registrar Componentes");
+		mnAdministrativo.add(mntmRegistrarComponentes);
+		
+		JMenuItem mntmRegistrarCombos = new JMenuItem("Registrar Combos");
+		mnAdministrativo.add(mntmRegistrarCombos);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
